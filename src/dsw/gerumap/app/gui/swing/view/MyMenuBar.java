195d560c54e6f86d34077d881693c907fcc57c43
BoskoZlatanovicRAF <bzlatanovic3621rn@raf.rs>
2022-10-23 19:15:16 +0200

    Doradjen Appcore, dodate funkcionalnosti za MainFrame i uradjen MyMenuBar

--- src/dsw/gerumap/app/gui/swing/view/MyMenuBar.java
+++ src/dsw/gerumap/app/gui/swing/view/MyMenuBar.java
@@ -1,10 +1,20 @@
 package dsw.gerumap.app.gui.swing.view;
 
 import javax.swing.*;
+import java.awt.event.KeyEvent;
 
-public class MyMenuBar {
-    private JMenuBar menuBar = new JMenuBar();
-    private JMenu menu1 = new JMenu("File");
+public class MyMenuBar extends JMenuBar{
+
+    public MyMenuBar() {
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+        fileMenu.add(MainFrame.getIntance().getActionManager().getInfoAction());
+        fileMenu.add(MainFrame.getIntance().getActionManager().getNewProjectAction());
+
+        this.add(fileMenu);
+    }
+
+
 
 
 }
