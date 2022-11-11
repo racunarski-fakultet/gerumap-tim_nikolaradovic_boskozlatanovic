3acf5a32a014d2f90e6cba77a0b89f8b0a3501a2
BoskoZlatanovicRAF <bzlatanovic3621rn@raf.rs>
2022-11-11 18:09:55 +0100

    Promene JTabbedPane

--- src/main/java/dsw/gerumap/app/gui/swing/tabbedPane/view/Tab.java
+++ src/main/java/dsw/gerumap/app/gui/swing/tabbedPane/view/Tab.java
@@ -8,11 +8,15 @@ import java.awt.*;
 
 public class Tab extends JTabbedPane {
     private MapNode mapNode;
-
+    private JPanel panel;
     public Tab(MapNode mapNode) {
+        super(JTabbedPane.TOP, JTabbedPane.WRAP_TAB_LAYOUT);
         this.mapNode = mapNode;
-        this.addTab(mapNode.getName(), null);
+
         this.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+        this.panel = new JPanel();
+        this.setBounds(50, 50, 250, 200);
+        this.addTab(mapNode.getName(), panel);
 
     }
 }
