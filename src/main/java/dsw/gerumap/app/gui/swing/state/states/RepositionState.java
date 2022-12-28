6f8be42e34d5ed335e3c3c235f317cd9f198c5e7
BoskoZlatanovicRAF <bzlatanovic3621rn@raf.rs>
2022-12-28 17:28:14 +0100

    bugfix za rasporedjivanje veza i glavnog pojma kad se crta veza od glavnog pojma ka obicnom

--- src/main/java/dsw/gerumap/app/gui/swing/state/states/RepositionState.java
+++ src/main/java/dsw/gerumap/app/gui/swing/state/states/RepositionState.java
@@ -46,47 +46,19 @@ public class RepositionState extends State {
 
                 double dist1 = Line2D.ptLineDist(0,tb.getHeight()/4,tb.getWidth(),tb.getHeight()/4,dp.getShape().getBounds2D().getCenterX(), dp.getShape().getBounds2D().getCenterY());
                 double dist2 = Line2D.ptLineDist(0,tb.getHeight()/1.2,tb.getWidth(),tb.getHeight()/1.2,dp.getShape().getBounds2D().getCenterX(), dp.getShape().getBounds2D().getCenterY());
+
                 int increment = 120;
 
-
-
                 if (dist1 < dist2){
-
-
                     dp.getElement().setX((float) (tb.getStart()+increment - ((PojamElement)dp.getElement()).getWidth()/2.f));
                     dp.getElement().setY((float) (tb.getHeight()/4 - ((PojamElement)dp.getElement()).getHeight()/2.f));
-                    float x1 = (float) dp.getElement().getX()+ ((PojamElement)dp.getElement()).getWidth()/2.f;
-                    float y1 = (float) dp.getElement().getY() + ((PojamElement)dp.getElement()).getHeight()/2.f;
-                    float x2 = (float) centralniPojam.getElement().getX() +  ((PojamElement)centralniPojam.getElement()).getWidth()/2.f;
-                    float y2 = (float) centralniPojam.getElement().getY() +  ((PojamElement)centralniPojam.getElement()).getHeight()/2.f;
-                    for(DevicePainter veza: ((PojamPainter) dp).getVeze()){
-                        Point point = new Point((int) ((VezaElement)veza.getElement()).getX2(), (int) ((VezaElement)veza.getElement()).getY2());
-                        if(centralniPojam.contains(point)){
-                            ((VezaElement)veza.getElement()).setX(x1);
-                            ((VezaElement)veza.getElement()).setY(y1);
-                            ((VezaElement)veza.getElement()).setX2(x2);
-                            ((VezaElement)veza.getElement()).setY2(y2);
-                        }
-                    }
+                    positionLine(dp, tb, centralniPojam);
                     tb.setStart(tb.getStart()+increment);
                 }
                 else {
-
                     dp.getElement().setX((float) (tb.getStart2()+increment - ((PojamElement)dp.getElement()).getWidth()/2.f));
                     dp.getElement().setY((float) (tb.getHeight()/1.2 - ((PojamElement)dp.getElement()).getHeight()/2.f));
-                    float x1 = (float) dp.getElement().getX() + ((PojamElement)dp.getElement()).getWidth()/2.f;
-                    float y1 = (float) dp.getElement().getY() + ((PojamElement)dp.getElement()).getHeight()/2.f;
-                    float x2 = (float) centralniPojam.getElement().getX() +  ((PojamElement)centralniPojam.getElement()).getWidth()/2.f;
-                    float y2 = (float) centralniPojam.getElement().getY() +  ((PojamElement)centralniPojam.getElement()).getHeight()/2.f;
-                    for(DevicePainter veza: ((PojamPainter) dp).getVeze()){
-                        Point point = new Point((int) ((VezaElement)veza.getElement()).getX2(), (int) ((VezaElement)veza.getElement()).getY2());
-                        if(centralniPojam.contains(point)){
-                            ((VezaElement)veza.getElement()).setX(x1);
-                            ((VezaElement)veza.getElement()).setY(y1);
-                            ((VezaElement)veza.getElement()).setX2(x2);
-                            ((VezaElement)veza.getElement()).setY2(y2);
-                        }
-                    }
+                    positionLine(dp, tb, centralniPojam);
                     tb.setStart2(tb.getStart2()+increment);
                 }
 
@@ -95,6 +67,22 @@ public class RepositionState extends State {
 
         }
         tb.repaint();
+    }
+
+    private void positionLine(DevicePainter dp, TabItemModel tb, PojamPainter centralniPojam){
+        float x1 = (float) dp.getElement().getX()+ ((PojamElement)dp.getElement()).getWidth()/2.f;
+        float y1 = (float) dp.getElement().getY() + ((PojamElement)dp.getElement()).getHeight()/2.f;
+        float x2 = (float) centralniPojam.getElement().getX() +  ((PojamElement)centralniPojam.getElement()).getWidth()/2.f;
+        float y2 = (float) centralniPojam.getElement().getY() +  ((PojamElement)centralniPojam.getElement()).getHeight()/2.f;
+        for(DevicePainter veza: ((PojamPainter) dp).getVeze()){
+            Point point = new Point((int) ((VezaElement)veza.getElement()).getX2(), (int) ((VezaElement)veza.getElement()).getY2());
+            if(centralniPojam.contains(point) || dp.contains(point)){
+                ((VezaElement)veza.getElement()).setX(x1);
+                ((VezaElement)veza.getElement()).setY(y1);
+                ((VezaElement)veza.getElement()).setX2(x2);
+                ((VezaElement)veza.getElement()).setY2(y2);
+            }
+        }
     }
 
 }
